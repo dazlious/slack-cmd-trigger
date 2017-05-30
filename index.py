9032a38f997f019035e6d787e6f35b75ad56fdbe
Michael Duve <mduve@designmail.net>
2017-05-30 12:07:17 +0200

    done

--- index.py
+++ index.py
@@ -5,7 +5,12 @@ settings = None
 
 def init_settings():
 	parser = argparse.ArgumentParser()
+
 	parser.add_argument('-t', '--token', help='Slack API token', required=True, type=str)
+	parser.add_argument('-c', '--channel', help='Slack channel', required=True, type=str)
+	parser.add_argument('-cmd', '--command', help='Slack command', required=True, type=str)
+	parser.add_argument('-i', '--instruction', help='Slack instruction for command', required=True, type=str)
+
 	args = parser.parse_args()
 	return args
 
@@ -15,6 +20,12 @@ def main():
 	try:
 		settings = init_settings()
 		slack = Slacker(settings.token)
+		channel_id = slack.channels.get_channel_id(settings.channel)
+		slack.chat.command(
+	        channel=channel_id,
+	        command='/' + settings.command,
+	        text=settings.instruction
+		)
 		
 	except (KeyboardInterrupt, SystemExit):
 		print "-> Aborted through user interaction"
